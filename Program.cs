c7b25c1ef28c4263cfb3fefe0d692db0a4d8b28e
Aina <aina@handcrafts.lv>
2025-06-12 17:51:17 +0300

    switch ar nosacījumu

--- Program.cs
+++ Program.cs
@@ -9,37 +9,18 @@ class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.InputEncoding = System.Text.Encoding.UTF8; // Ieteicams arī ievadei
 
-        Console.WriteLine("Pāra vai nepāra skaitļa noteikšana:");
-        Console.WriteLine("--------------------------------------------");
-
-        int skaitlis;
-        bool ievadeDeriga;
-
-        // Nepārtraukti prasām ievadi, līdz lietotājs ievada derīgu veselu skaitli
-        do
+        int skaitlis = 25;
+        switch (skaitlis)
         {
-            Console.Write("Lūdzu, ievadiet veselu skaitli: ");
-            string ievadeTeksts = Console.ReadLine();
-
-            // Mēģinām pārvērst tekstu par int, izmantojot int.TryParse()
-            ievadeDeriga = int.TryParse(ievadeTeksts, out skaitlis);
-
-            if (!ievadeDeriga)
-            {
-                Console.WriteLine("Kļūda: Nederīga ievade. Lūdzu, ievadiet derīgu veselu skaitli.");
-            }
-        } while (!ievadeDeriga);
-
-        // Pārbaudām, vai skaitlis ir pāra vai nepāra, izmantojot % operatoru
-        // Ja skaitli dala ar 2 un atlikums ir 0, tad tas ir pāra skaitlis.
-        // Pretējā gadījumā tas ir nepāra skaitlis.
-        if (skaitlis % 2 == 0)
-        {
-            Console.WriteLine($"Skaitlis {skaitlis} ir pāra skaitlis.");
-        }
-        else
-        {
-            Console.WriteLine($"Skaitlis {skaitlis} ir nepāra skaitlis.");
+            case int n when n < 10:
+                Console.WriteLine("Mazs skaitlis");
+                break;
+            case int n when n > 20 && n < 40:
+                Console.WriteLine("Vidējs skaitlis");
+                break;
+            default:
+                Console.WriteLine("Cits skaitlis");
+                break;
         }
 
         // Gaidām, kamēr lietotājs nospiež jebkuru taustiņu, lai konsole neaizvērtos uzreiz
